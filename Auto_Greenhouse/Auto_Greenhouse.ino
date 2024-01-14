c87f2f33f10dc69f0125ddfae7a5319823b30092
Simon Deslauriers <simdeslau@gmail.com>
2024-01-14 16:45:17 -0500

    I2C protocole for LCD display

--- Auto_Greenhouse/Auto_Greenhouse.ino
+++ Auto_Greenhouse/Auto_Greenhouse.ino
@@ -1,17 +1,28 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+//Variables for moisture sensors
 const int dry = 480;
 const int wet = 198;
 
 
 void setup() {
  Serial.begin(9600);
+
+ lcd.init();
+ lcd.backlight();
 }
 
 void loop() {
-  int sensorVal = analogRead(A0);
-  int percentageHumidity = map(sensorVal, wet, dry, 100, 0);
-
+  //soil moisture
+  int MS_Val = analogRead(A0);
+  int percentageHumidity = map(MS_Val, wet, dry, 100, 0);
   Serial.print(percentageHumidity);
   Serial.println("%");
-  
+  lcd.setCursor(0, 0);
+  lcd.print("Moisture:");
+  lcd.print(percentageHumidity);
+  lcd.print("%");
   delay(100); 
 }
