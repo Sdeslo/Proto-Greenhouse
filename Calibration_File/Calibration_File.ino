084bc5ec35f8f75473b360fa6d3488a68ae83fde
Simon Deslauriers <simdeslau@gmail.com>
2024-01-15 15:53:48 -0500

    Added instuctions
    Added instructions in calibration file

--- Calibration_File/Calibration_File.ino
+++ Calibration_File/Calibration_File.ino
@@ -1,3 +1,4 @@
+// Replace the values by the values obtained
 const int dry = 480;
 const int wet = 198;
 
@@ -7,6 +8,8 @@ void setup() {
 }
 
 void loop() {
+  // MS calibration
+  // A0 in this case because sensor is connected to the pin Analog 0
   int sensorVal = analogRead(A0);
 
   Serial.print(sensorVal);
