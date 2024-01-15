1ade6f63212520cffbf9422f1db47daa9cf2c729
Simon Deslauriers <simdeslau@gmail.com>
2024-01-14 20:08:53 -0500

    I2C updates
    Added a I2C adress scanner and updated the code to display on the LCD

--- Auto_Greenhouse/Auto_Greenhouse.ino
+++ Auto_Greenhouse/Auto_Greenhouse.ino
@@ -1,9 +1,8 @@
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 //Variables for moisture sensors
-const int dry = 480;
+const int dry = 482;
 const int wet = 198;
 
 
@@ -22,7 +21,10 @@ void loop() {
   Serial.println("%");
   lcd.setCursor(0, 0);
   lcd.print("Moisture:");
+  lcd.setCursor(0, 1);
   lcd.print(percentageHumidity);
+  lcd.setCursor(2, 1);
   lcd.print("%");
-  delay(100); 
+  delay(1000); 
+  lcd.clear();
 }
