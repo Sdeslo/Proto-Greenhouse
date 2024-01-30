271767a648bb3b62e1f2dddf28c559272de50121
Simon Deslauriers <sides110@ulaval.ca>
2024-01-29 23:31:33 -0500

    Added Water Level

--- Auto_Greenhouse/Auto_Greenhouse.ino
+++ Auto_Greenhouse/Auto_Greenhouse.ino
@@ -4,7 +4,11 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 //Variables for moisture sensors
 const int dry = 482;
 const int wet = 198;
+int MS_Val = analogRead(A0);
+int percentageHumidity = map(MS_Val, wet, dry, 100, 0);
 
+//Variables for water level
+in WL_VAL = digitalRead(D8)
 
 void setup() {
  Serial.begin(9600);
@@ -15,16 +19,19 @@ void setup() {
 
 void loop() {
   //soil moisture
-  int MS_Val = analogRead(A0);
-  int percentageHumidity = map(MS_Val, wet, dry, 100, 0);
-  Serial.print(percentageHumidity);
-  Serial.println("%");
   lcd.setCursor(0, 0);
   lcd.print("Moisture:");
   lcd.setCursor(0, 1);
   lcd.print(percentageHumidity);
   lcd.setCursor(2, 1);
   lcd.print("%");
+
+  //Water level sensor
+  lcd.setCursor(0, 2);
+  if (WL_Val == LOW) {
+    lcd. print("Low")
+  }
+  
   delay(1000); 
   lcd.clear();
 }
