ccb7fc2b2676db1a2e1319a98fdda736a67c2053
Simon Deslauriers <simdeslau@gmail.com>
2024-02-18 20:41:21 -0500

    Working Water System
    Added relay activation for pump and water level sensor

--- Auto_Greenhouse/Auto_Greenhouse.ino
+++ Auto_Greenhouse/Auto_Greenhouse.ino
@@ -1,24 +1,31 @@
 #include <LiquidCrystal_I2C.h>
+
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-//Variables for moisture sensors
+// Variables for moisture sensors
 const int dry = 482;
 const int wet = 198;
-int MS_Val = analogRead(A0);
-int percentageHumidity = map(MS_Val, wet, dry, 100, 0);
 
-//Variables for water level
-in WL_VAL = digitalRead(D8)
+// Variables for water pump
+const int relayPin = 7;
 
 void setup() {
- Serial.begin(9600);
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
 
- lcd.init();
- lcd.backlight();
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
-  //soil moisture
+  // Variables for moisture sensors
+  const int MS_Val = analogRead(A0);
+  const int percentageHumidity = map(MS_Val, wet, dry, 100, 0);
+
+  // Variables for water level
+  const int WL_Val = digitalRead(D8);
+
+  // Soil moisture
   lcd.setCursor(0, 0);
   lcd.print("Moisture:");
   lcd.setCursor(0, 1);
@@ -26,12 +33,21 @@ void loop() {
   lcd.setCursor(2, 1);
   lcd.print("%");
 
-  //Water level sensor
+  // Water level sensor
   lcd.setCursor(0, 2);
-  if (WL_Val == LOW) {
-    lcd. print("Low")
+  if (WL_Val == LOW)
+  {
+    lcd.print("Low Water");
   }
-  
-  delay(1000); 
+
+  // Relay Activation
+  if (percentageHumidity < 10)
+  {
+    digitalWrite(relayPin, HIGH);
+    lcd.setCursor(12, 0);
+    lcd.print("Pump ON");
+  }
+
+  delay(2000);
   lcd.clear();
 }
